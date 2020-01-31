5697bf6c15191c2d054c9676ce77621db4b8bae5
Alex <Alex@DESKTOP-CEQG6CC>
2020-01-31 19:18:15 +0200

    eND

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -7,6 +7,7 @@
 			Element* pNext;
 			static int count; //статическая переменная (инициализируется за классом)
 		public:
+			/************************* CONSTRUCTORS ***************************/
 			Element(int Data, Element* pNext = nullptr)
 			{
 				this->Data = Data;
@@ -14,6 +15,7 @@
 				count++;
 				std::cout << "EConstructor:\t" << this << std::endl;
 			}
+			/************************* DESTRUCTOR ***************************/
 			~Element()
 			{
 				count--;
@@ -25,28 +27,39 @@
 
 		Element* Head;
 	public:
+/************************* CONSTRUCTORS ***************************/
 		Forwardlist()
 		{
 			this->Head = nullptr;
 			std::cout << "LConstructor:\t" << this << std::endl;
 		}
+/************************* DESTRUCTOR ***************************/
 		~Forwardlist()
-		{			
-			//Element* Temp = Head; 
-			//while (Temp)
-			//{
-			//	Temp = Temp->pNext;// Переход на следующий элемент
-			//}
+		{				
+			Element* Temp = Head;
+			Element* Old = Head;
+			
+			while (Temp->pNext != nullptr)
+			{
+				Old = Temp;
+				Temp = Temp->pNext;
+				delete Old;
+			}			
+			delete Temp;
 			std::cout << "LDestructor:\t" << this << std::endl;
 		}
-////////////////////////////////////////////////////////////////////////////
+
+		
+/************************* METODS ***************************/
+		//ДОБАВЛЕНИЕ ЭЛЕМЕНТА В НАЧАЛО СПИСКА
 		void push_front(int Data)
 		{
 			Element* New = new Element(Data);
 			New->pNext = Head;
 			Head = New;
 		}
-		// Добавление элемента в конец списка
+
+		//ДОБАВЛЕНИЕ ЭЛЕМЕНТА В КОНЕЦ СПИСКА
 		void push_back(int Data)
 		{
 			Element* Temp = Head; 
@@ -57,18 +70,8 @@
 			Element* New = new Element(Data);
 			Temp->pNext = New;
 		}
-		// metods
-		void print()
-		{
-			Element* Temp = Head; // Temp - итератор(указатель при помощи которого можно получить 
-			//доступ к элементам структуры данных)
-			while (Temp)
-			{
-				std::cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
-				Temp = Temp->pNext;// Переход на следующий элемент
-			}
-		}
-		// удаление элемента из начала списка
+
+		// УДАЛЕНИЕ ЭЛЕМЕНТА С НАЧАЛА СПИСКА
 		void pop_front()
 		{
 			Element* Temp = Head; 
@@ -76,18 +79,26 @@
 			delete Temp;
 		}
 
-		//// удаление элемента из конца списка
-		//void pop_back()
-		//{
-		//	Element* Temp = Head;
-		//	while(Temp->pNext != nullptr)
-		//	{
+		// УДАЛЕНИЕ ЭЛЕМЕНТА ИЗ КОНЦА СПИСКА
+		void pop_back()
+		{
+			Element* Old = Head;
+			while(Old->pNext != nullptr)
+			{
+				Old = Old->pNext;
+			}
+			Element* Temp = Head;
+			while (Temp->pNext != Old)
+			{
+				Temp = Temp->pNext;
+			}
 
-		//	}
-		//	
-		//	delete Temp;
-		//}
+			Temp->pNext = nullptr;
+			delete Old;
+			
+		}
 
+		//ДОБАВЛЕНИЕ ПО ИНДЕКСУ
 		void insert(int index, int Data)
 		{
 			if(index == 0)
@@ -122,36 +133,87 @@
 			New->pNext = Temp->pNext;
 			Temp->pNext = New;
 		}
-		void erase(int index) // HomeWork удаление по индексу
+
+		//УДАЛЕНИЕ ПО ИНДЕКСУ
+		void erase(int index) 
 		{
+			Element* Old = Head;
+			Element* Temp = Head;
+		    if (index == 0)
+			{
+				Head = Head->pNext;
+				delete Old;
+				return;
+			}			
 
+			int y = 0;
+			while (Old->pNext != nullptr)
+			{
+				Old = Old->pNext;
+				y++;
+			}
+
+			if (index >= y)
+			{
+				while (Temp->pNext != Old)
+				{
+					Temp = Temp->pNext;
+				}
+				Temp->pNext = nullptr;
+				delete Old;
+				return;
+			}
+			
+			Old = Head;
+			for (int i = 0; i < index; i++)
+			{
+				Old = Old->pNext;
+			}
+			while (Temp->pNext != Old)
+			{
+				Temp = Temp->pNext;
+			}
+			Temp->pNext = Old->pNext;
+			delete Old;
 		}
-		//Коректный деструктор для элементов
-	};
 
-	int Forwardlist::Element::count = 0;// инициализация статической переменной
+		//ВЫВОД СТРУКТУРЫ НА ЭКРАН
+		void print()
+		{
+			Element* Temp = Head; // Temp - итератор(указатель при помощи которого можно получить 
+			//доступ к элементам структуры данных)
+			while (Temp)
+			{
+				std::cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
+				Temp = Temp->pNext;// Переход на следующий элемент
+			}
+		}
+	};
+#define DinDataStr
+
+int Forwardlist::Element::count = 0;// инициализация статической переменной
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+#ifdef DinDataStr
 	int n;
 	std::cout << "Input List size:"; std::cin >> n;
 	Forwardlist fl;
-	
 	for (int i = 0; i < n; i++)
 	{
 		fl.push_front(rand() % 100);
 	}
-	fl.push_back(123);
+		
+	fl.print();	
+	std::cout << "-----------------POP_BACK----------------" << std::endl;
+	fl.pop_back();
 	fl.print();
-	/*std::cout << "--------------------------------------" << std::endl;
-	fl.pop_front();
-	fl.print();*/
-	std::cout << "--------------------------------------" << std::endl;
+	std::cout << "-----------------ERASE----------------" << std::endl;
 	int index;
-	int data;
 	std::cout << "Input Index: "; std::cin >> index;
-	std::cout << "Input value: "; std::cin >> data;
-	fl.insert(index, data);
+	fl.erase(index);
 	fl.print();
+#endif // DinDataStr
+	
 }
